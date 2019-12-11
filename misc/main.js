24686520b258ff6a14906d48e195e530ec4b0857
oox <oiotoxt@gmail.com>
2019-12-11 20:57:11 +0900

    Trivial

--- misc/main.js
+++ misc/main.js
@@ -1,4 +1,4 @@
-C =   [      1,       7,      28,      84,     210,     462,     924,
+var C =   [      1,       7,      28,      84,     210,     462,     924,
           1716,    3003,    5005,    8002,   12334,   18396,   26628,
          37500,   51492,   69069,   90651,  116578,  147070,  182197,
         221859,  265776,  313488,  364365,  417627,  472374,  527626,
@@ -10,26 +10,20 @@ C =   [      1,       7,      28,      84,     210,     462,     924,
 
 // [3.5 대 1] 이면 rate = 3.5
 function get_probable_num(rate) {
-    if (rate < 1.0)
-        rate = 1.0;
+    if (rate < 1.0) rate = 1.0;
 
     percentile = 1.0 - (1.0 / rate);
     document.write("<P>Target percentile : " + percentile * 100.0 + "+</P>");
 
-    if (percentile < 0)
-        return 0;
-    if (percentile >= 1)
-        return C.length - 1;
+    if (percentile < 0) return 0;
+    if (percentile >= 1) return C.length - 1;
 
     for (i=0; i<C.length; ++i)
         if (percentile <= C[i] / 1e6)
             return i;
-
-    alert("hmm")
 }
 
 function show_probable_num(rate) {
-    // 필요한 숫자 합 계산
     v = get_probable_num(rate);
     document.write("<P>당첨되려면 아마도 " + v + " 이상이 필요할거에요~</P>");
 }
