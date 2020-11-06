4f8534a4d21da3a0113865892b7dc50e70e23585
oox <oiotoxt@gmail.com>
2020-11-06 15:27:38 +0900

    Cleanup

--- app_dash.py
+++ app_dash.py
@@ -1,9 +1,12 @@
+#
+# 돌아만 가도록 급조된 코드입니다;;
+#
+
 # refs:
 #   https://dash.plotly.com/dash-core-components/graph
 #   https://dash.plotly.com/basic-callbacks
 #   https://dash.plotly.com/live-updates
 
-import numpy as np
 import plotly.express as px
 import dash
 from dash.dependencies import Input
@@ -15,6 +18,9 @@ import dash_html_components as html
 import prob_short as prob
 
 
+_mark = [0, 400, 600, 800, 900, 1000]
+_conv = [0, 10, 100, 1000, 10000, 1000000]
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -23,12 +29,12 @@ fig_pdf = px.bar(y=prob.PDF,
                  labels={'x': '숫자 합', 'y': '전체 백만 명 중 사람 수'})
 
 fig_pdf.add_shape(type="rect",
-    x0=0, y0=0, x1=27, y1=60000,
-    line=dict(
-        width=0,
-    ),
-    fillcolor='rgba(255, 0, 0, 0.2)',
-)
+                  x0=0, y0=0, x1=27, y1=60000,
+                  line=dict(
+                      width=0,
+                  ),
+                  fillcolor='rgba(255, 0, 0, 0.2)',
+                  )
 
 fig_pdf.update_layout(
     # title_text='백만 명이 추첨에 참여한 경우',
@@ -43,33 +49,8 @@ fig_cdf.update_layout(
     xaxis=dict(tickmode='linear')
 )
 
-def convert_slider_value(val):
-    mark = [200, 400, 600, 800, 1000]
-    conv = [10, 100, 1000, 10000, 1000000]
-    if val <= 2.0:
-        val = 1.0
-    elif val <= mark[0]:
-        val /= mark[0] / 10
-    elif val <= mark[1]:
-        val = ((val - mark[0]) / (mark[1] - mark[0]) * (conv[1] - conv[0])) + conv[0]
-        val = int(val)
-    elif val <= mark[2]:
-        val = ((val - mark[1]) / (mark[2] - mark[1]) * (conv[2] - conv[1])) + conv[1]
-        val = int(val)
-    elif val <= mark[3]:
-        val = ((val - mark[2]) / (mark[3] - mark[2]) * (conv[3] - conv[2])) + conv[2]
-        val = int(val)
-    elif val <= mark[4]:
-        val = ((val - mark[3]) / (mark[4] - mark[3]) * (conv[4] - conv[3])) + conv[3]
-        val = int(val)
-    return val
-
 app.layout = html.Div(children=[
     html.Center([
-        # html.H1(children='당첨 숫자 예측',
-        #         style={'margin-top': '40px', 'margin-bottom': '60px'}
-        #         ),
-
         html.Br(),
 
         dcc.Markdown('''
@@ -79,7 +60,11 @@ app.layout = html.Div(children=[
         html.Br(),
 
         dcc.Markdown('''
-            ```경쟁률이 [3.5 대 1]이라면 [3.5]를 입력해 주세요```
+            ```경쟁률이 [3.5 대 1] 이라면 [3.5]를 입력하시거나,```
+        '''),
+
+        dcc.Markdown('''
+            ```아래 <슬라이드바>에서 [3.5]를 선택해 주세요.```
         '''),
 
         html.Div(id='output-warning',
@@ -94,59 +79,40 @@ app.layout = html.Div(children=[
             max=1e6,
             # step=0.05,
             value=2.0,
-            style={'textAlign': 'center', 'margin-bottom': '0px'}
+            style={'textAlign': 'center', 'margin-bottom': '40px'}
         ),
-
-        html.Div(id='output-msg1',
-                 style={'margin-top': '20px','margin-bottom': '20px'}
-                 ),
-
-        # dcc.Slider(
-        #     id='my-slider',
-        #     min=1,
-        #     max=1e6,
-        #     step=0.05,
-        #     # tooltip={'always_visible': True},
-        #     marks={
-        #         1: '1',
-        #         200000: '10',
-        #         400000: '100',
-        #         600000: '1000',
-        #         800000: '10000',
-        #         1000000: '1000000',
-        #     },
-        #     value=2.0,
-        #     updatemode='drag'
-        # ),
 
         dcc.Slider(
             id='my-slider',
-            min=20,
+            min=int(_mark[1]*0.1),
             max=1000,
             step=1,
             # tooltip={'always_visible': True},
             marks={
-                20: '1',
-                200: '10',
-                400: '100',
-                600: '1000',
-                800: '10000',
-                1000: '1000000',
+                int(_mark[1]*0.1): '1',
+                _mark[1]: str(_conv[1]),
+                _mark[2]: str(_conv[2]),
+                _mark[3]: str(_conv[3]),
+                _mark[4]: str(_conv[4]),
+                _mark[5]: str(_conv[5]),
             },
             value=40.0,
             updatemode='drag'
         ),
 
-        html.Div(id='output-msg2',
-                 style={ 'font-weight': 'bold',
+        html.Div(id='output-predict',
+                 style={'font-weight': 'bold',
                         'font-size': '2.0em',
-                     'margin-bottom': '40px'}
+                        'margin-top': '60px',
+                        'margin-bottom': '0px'}
                  ),
 
-        # dcc.Graph(id='live-update-graph'),
+        html.Div(id='output-percentile',
+                 style={'margin-top': '0px', 'margin-bottom': '0px'}
+                 ),
 
         dcc.Graph(
-            id='graph pdf',
+            id='graph-pdf',
             figure=fig_pdf,
             # config={
             #     'editable': True,
@@ -157,7 +123,7 @@ app.layout = html.Div(children=[
         ),
 
         # dcc.Graph(
-        #     id='graph cdf',
+        #     id='graph-cdf',
         #     figure=fig_cdf
         # ),
 
@@ -170,10 +136,16 @@ def rate_to_percentile(rate):
     return 1.0 - (1.0 / rate)
 
 
+def convert_slider_value(val):
+    for idx in range(1, len(_mark)):
+        if val <= _mark[idx]:
+            return (val - _mark[idx-1]) * (_conv[idx] - _conv[idx-1]) / (_mark[idx] - _mark[idx-1]) + _conv[idx-1]
+
+
 @app.callback(
-    Output('graph pdf', 'figure'),
+    Output('graph-pdf', 'figure'),
     [Input('rate', 'value')],
-    [State('graph pdf', 'figure')])
+    [State('graph-pdf', 'figure')])
 def update_output_input(val, fig):
     if val is not None:
         fig['layout']['shapes'][0]['x1'] = prob.predict(val)
@@ -184,14 +156,13 @@ def update_output_input(val, fig):
     Output('rate', 'value'),
     [Input('my-slider', 'value')])
 def update_output_slider(value):
-    # print('``````````````````````````', value)
     return convert_slider_value(value)
 
 
 @app.callback(
     [Output('output-warning', 'children'),
-    Output('output-msg1', 'children'),
-    Output('output-msg2', 'children')],
+     Output('output-percentile', 'children'),
+     Output('output-predict', 'children')],
     [Input('rate', 'value')])
 def update_output_guess(value):
     if value is None:
